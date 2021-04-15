a76c32fbd1c4ab94baf70a375939cd2ad906633f
jeffrey <1694717830@qq.com>
2021-04-15 18:21:26 +0800

    fix package

--- src/Equipment/Terminal.php
+++ src/Equipment/Terminal.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace UsefulWidgets\Equipment;
+namespace Practical\Equipment;
 
 /**
  *      ┌─┐       ┌─┐ + +
--- src/Tool/CurlHttp.php
+++ src/Tool/CurlHttp.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace UsefulWidgets\Tool;
+namespace Practical\Tool;
 
 /**
  *             ,%%%%%%%%,
