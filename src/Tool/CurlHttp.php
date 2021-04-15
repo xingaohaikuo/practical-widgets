ec2fef03b0b6f77f9eabf85bb701508b5bf3723c
jeffrey <1694717830@qq.com>
2021-04-15 17:56:52 +0800

    git init

---
+++ src/Tool/CurlHttp.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace UsefulWidgets\Tool;
+
+/**
+ *             ,%%%%%%%%,
+ *           ,%%/\%%%%/\%%
+ *          ,%%%\c "" J/%%%
+ * %.       %%%%/ o  o \%%%
+ * `%%.     %%%%    _  |%%%
+ *  `%%     `%%%%(__Y__)%%'
+ *  //       ;%%%%`\-/%%%'
+ * ((       /  `%%%%%%%'
+ *  \\    .'          |
+ *   \\  /       \  | |
+ *    \\/         ) | |
+ *     \         /_ | |__
+ *     (___________))))))) 攻城湿
+ *
+ *        _       _
+ * __   _(_)_   _(_) __ _ _ __
+ * \ \ / / \ \ / / |/ _` |'_ \
+ *  \ V /| |\ V /| | (_| | | | |
+ *   \_/ |_| \_/ |_|\__,_|_| |_|
+ * 
+ * curl请求封装
+ */
+class CurlHttp
+{
+    private $ch ;
+    private $url ;
+    private $method ;
+    private $params ;
+    private $timeout;
+    protected $multipart ;
+
+    public function __construct()
+    {
+        $this->timeout = 120 ;
+    }
+
+    public function Get($data)
+    {
+        $data['method'] = "GET";
+        return $this->performRequest($data);
+    }
+
+    public function Post($data)
+    {
+        $data['method'] = "POST";
+        return $this->performRequest($data);
+    }
+
+    public function Put($data)
+    {
+        $data['method'] = "PUT";
+        return $this->performRequest($data);
+    }
+
+    public function Delete($data)
+    {
+        $data['method'] = "DELETE";
+        return $this->performRequest($data);
+    }
+
+    public function Upload($data)
+    {
+        $data['method'] = "POST";
+        $this->multipart = true;
+        return $this->performRequest($data);
+    }
+
+    /**
+     * Http 请求
+     * @param $data
+     * @return array
+     */
+    public function performRequest($data)
+    {
+        $this->ch = curl_init();
+        $url = $data['url'];
+        try {
+            $this->dataValication($data);
+        } catch (\Exception $e) {
+            return ['code'=>-1, 'msg'=>$e->getMessage()];
+        }
+
+        $timeout = isset($data['timeout'])?$data['timeout']:$this->timeout;
+        $headers = $this->setHeaders($data);
+        curl_setopt($this->ch, CURLOPT_TIMEOUT, $timeout);
+        curl_setopt($this->ch, CURLOPT_HEADER, true);
+        curl_setopt($this->ch, CURLINFO_HEADER_OUT, true);
+        if (!empty($headers)) {
+            curl_setopt($this->ch, CURLOPT_HTTPHEADER, $headers);
+        }
+        curl_setopt($this->ch, CURLOPT_NOBODY, false);
+        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($this->ch, CURLOPT_CONNECTTIMEOUT, $this->timeout);
+        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, $this->method); //设置请求方式
+
+        if ($this->method=="GET") {
+            if(strpos($this->url,'?')){
+                $this->url .= http_build_query($this->params);
+            }else{
+                $this->url .= '?' . http_build_query($this->params);
+            }
+        }else{
+            curl_setopt($this->ch, CURLOPT_POSTFIELDS, $this->multipart?$this->params:http_build_query($this->params));
+        }
+
+        curl_setopt($this->ch, CURLOPT_URL, $this->url);
+
+        if (1 == strpos('$'.$this->url, "https://")) {
+            curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, false);
+        }
+        $result = curl_exec($this->ch);
+
+
+        if(!curl_errno($this->ch)){
+            list($response_header, $response_body) = explode("\r\n\r\n", $result, 2);
+            $contentType = curl_getinfo($this->ch, CURLINFO_CONTENT_TYPE);
+
+            $info = curl_getinfo($this->ch);
+            $response = ['code'=>0, 'msg'=>'OK', 'data'=>json_decode($response_body, true), 'contentType'=>$contentType];
+        }else{
+            $response = ['data'=>['code'=>-1, 'msg'=>"请求 $url 出错: Curl error: ". curl_error($this->ch)]];
+        }
+
+        curl_close($this->ch);
+
+        return $response;
+    }
+
+    /**
+     * 设置Header信息
+     * @param $data
+     * @return array
+     */
+    public function setHeaders($data)
+    {
+        $headers = array();
+        if (isset($data['headers'])) {
+            foreach ($data['headers'] as $key=>$item) {
+                $headers[] = "$key:$item";
+            }
+        }
+        $headers[] = "Expect:"; // post数据大于1k时，默认不需要添加Expect:100-continue
+
+        return $headers;
+    }
+
+    public function setTimeout($timeout)
+    {
+        if (!empty($timeout) || $timeout != 30) {
+            $this->timeout = $timeout ;
+        }
+
+        return $this;
+    }
+
+    /**
+     * 数据验证
+     * @param $data
+     * @throws \Exception
+     */
+    public function dataValication($data)
+    {
+        if(!isset($data['url']) || empty($data['url'])){
+            throw new \Exception("HttpClient Error: Uri不能为空", 4422);
+        }else{
+            $this->url = $data['url'];
+        }
+
+        if(!isset($data['params']) || empty($data['params'])){
+            $this->params = [];
+        }else{
+            $this->params = $data['params'];
+        }
+
+        if(!isset($data['method']) || empty($data['method'])){
+            $this->method = "POST";
+        }else{
+            $this->method = $data['method'];
+        }
+    }
+
+}
---
+++ src/Tool/Kernel.php
@@ -0,0 +1,215 @@
+<?php
+
+namespace UsefulWidgets\Tool;
+
+/**
+ * 
+ *                    _ooOoo_
+ *                   o8888888o
+ *                   88" . "88
+ *                   (| -_- |)
+ *                    O\ = /O
+ *                ____/`---'\____
+ *              .   ' \\| |// `.
+ *               / \\||| : |||// \
+ *             / _||||| -:- |||||- \
+ *               | | \\\ - /// | |
+ *             | \_| ''\---/'' | |
+ *              \ .-\__ `-` ___/-. /
+ *           ___`. .' /--.--\ `. . __
+ *        ."" '< `.___\_<|>_/___.' >'"".
+ *       | | : `- \`.;`\ _ /`;.`/ - ` : | |
+ *         \ \ `-. \_ __\ /__ _/ .-` / /
+ * ======`-.____`-.___\_____/___.-`____.-'======
+ *                    `=---='
+ *
+ * .............................................
+ *          佛祖保佑             永无BUG
+ */
+class Kernel
+{
+
+    /**
+     * 密码至少包含字母数字符号中的两种，6-12位
+     * @param string $passwd
+     * @return bool
+     */
+    public function checkPassword($passwd)
+    {
+        $str = $this->chars();
+        if (preg_match("/(^(?=.*\d)(?=.*[A-Za-z])[\da-zA-Z{$str}]{6,12}$)|(^(?=.*\d)(?=.*[{$str}])[\da-zA-Z{$str}]{6,12}$)|(^(?=.*[A-Za-z])(?=.*[{$str}])[\da-zA-Z{$str}]{6,12}$)|(^(?=.*\d)(?=.*[A-Za-z])(?=.*[{$str}])[\da-zA-Z{$str}]{6,12}$)/", $passwd)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 特殊符号
+     */
+    public function chars()
+    {
+        return "~!@#$%^&*()_+`\-={}\[\]:\";'<>?,.\/";
+    }
+
+    /**
+     * 过滤空格换行符
+     * @param string $str
+     * @return string
+     */
+    public function myTrim($str)
+    {
+        $search = array(" ", "　", "\n", "\r", "\t");
+        $replace = array("", "", "", "", "");
+        return str_replace($search, $replace, $str);
+    }
+
+    /**
+     * 获取时间段
+     * @param string $name
+     * @return array
+     */
+    public function getTimestamp($name = 'today')
+    {
+        $time = time();
+        switch ($name) {
+            case 'today': //今天
+                $timestamp['start'] = mktime(0, 0, 0, date('m'), date('d'), date('Y'));
+                $timestamp['end'] = mktime(23, 59, 59, date('m'), date('d'), date('Y'));
+                break;
+            case 'week': //本周
+                $timestamp['start'] = strtotime(date('Y-m-d', strtotime("this week Monday", $time)));
+                $timestamp['end'] = strtotime(date('Y-m-d', strtotime("this week Sunday", $time))) + 24 * 3600 - 1;
+                break;
+            case 'month': //本月
+                $timestamp['start'] = mktime(0, 0, 0, date('m'), 1, date('Y'));
+                $timestamp['end'] = mktime(23, 59, 59, date('m'), date('t'), date('Y'));
+                break;
+            case 'yesterday': //昨天
+                $yesterday = date('d') - 1;
+                $timestamp['start'] = mktime(0, 0, 0, date('m'), $yesterday, date('Y'));
+                $timestamp['end'] = mktime(23, 59, 59, date('m'), $yesterday, date('Y'));
+                break;
+            case 'permonth': //上月
+                $timestamp['start'] = mktime(0, 0, 0, date('m') - 1, 1, date('Y'));
+                $timestamp['end'] = mktime(23, 59, 59, date('m') - 1, date('t', $timestamp['start']), date('Y'));
+                break;
+            case 'preweek': //上周
+                $timestamp['start'] = strtotime(date('Y-m-d', strtotime("last week Monday", $time)));
+                $timestamp['end'] = strtotime(date('Y-m-d', strtotime("last week Sunday", $time))) + 24 * 3600 - 1;
+                break;
+            default:
+                $timestamp['start'] = mktime(0, 0, 0, date('m'), date('d'), date('Y'));
+                $timestamp['end'] = mktime(23, 59, 59, date('m'), date('d'), date('Y'));
+                break;
+        }
+
+        return $timestamp;
+    }
+
+    /**
+     * 中文、字母、数字组合匹配
+     * @param $chars
+     * @param string $encoding
+     * @return string
+     */
+    public function matchChinese($chars, $encoding = 'utf8')
+    {
+        $pattern = ($encoding == 'utf8') ? '/[\x{4e00}-\x{9fa5}a-zA-Z0-9]/u' : '/[\x80-\xFF]/';
+        preg_match_all($pattern, $chars, $result);
+        $temp = join('', $result[0]);
+        return $temp;
+    }
+
+    /**
+     * 手机号中间四位替换为*号
+     * @param $str
+     * @return null|string|string[]
+     */
+    public function switchPhone($str)
+    {
+        $pattern = '/(\d{3})(\d{4})(\d{4})/i';
+        $replacement = '$1****$3';
+        $resstr = preg_replace($pattern, $replacement, $str);
+        return $resstr;
+    }
+
+    /**
+     * 微信、邮箱、手机账号中间字符串以*隐藏
+     */
+    public function hideStr($str)
+    {
+        if (strpos($str, '@')) {
+            $email_array = explode("@", $str);
+            //邮箱前缀
+            $prevfix = (strlen($email_array[0]) < 4) ? "" : substr($str, 0, 3);
+            $count = 0;
+            $str = preg_replace('/([\d\w+_-]{0,100})@/', '***@', $str, -1, $count);
+            $rs = $prevfix . $str;
+        } else {
+            //正则手机号
+            $pattern = '/(1[23456789]{1}[0-9])[0-9]{4}([0-9]{4})/i';
+            if (preg_match($pattern, $str)) {
+                $rs = preg_replace($pattern, '$1****$2', $str); // substr_replace($name,'****',3,4);
+            } else {
+                $rs = substr($str, 0, 3) . "***"; //. substr($str, -1);
+            }
+        }
+        return $rs;
+    }
+
+    /**
+     * @Description: 毫秒时间戳
+     * @param {*}
+     * @return {*}
+     */
+    public function msecTime()
+    {
+        list($msec, $sec) = explode(' ', microtime());
+        $msecTime = (float)sprintf('%.0f', (floatval($msec) + floatval($sec)) * 1000);
+        return $msecTime;
+    }
+
+    /**
+     * 导出功能
+     * @param array $head = array('名称','ID') //表头
+     * @param array $export_data = array(['name'=>'测试1', 'id'=>1], ['name'=>'测试2', 'id'=>2])
+     */
+    public function putCsv(array $head, $export_data, $fileName = "test.csv")
+    {
+        $fileName = mb_convert_encoding($fileName . '.csv', "GBK", "UTF-8");
+        header("Content-type:application/vnd.ms-excel;charset=utf-8");
+        header("Content-Disposition:attachment;filename=$fileName");
+        // 打开PHP文件句柄，php://output 表示直接输出到浏览器
+        $fp = fopen('php://output', 'a');
+        fwrite($fp, chr(0xEF) . chr(0xBB) . chr(0xBF)); //输出BOM头
+        // 将中文标题转换编码，否则乱码
+        foreach ($head as $i => $v) {
+            $string = mb_detect_encoding($v, array("ASCII", "UTF-8", "GB2312", "GBK", "BIG5"));
+            $column_name[$i] = iconv($string, "GBK", $v);
+        }
+        // 将标题名称通过fputcsv写到文件句柄
+        fputcsv($fp, $column_name);
+        $cnd = 0;
+        foreach ($export_data as $item) {
+            $cnd++;
+            if ($cnd == 10000) {
+                ob_flush();
+                flush();
+                $cnd = 0;
+            }
+            $rows = array();
+            foreach ($item as $value) {
+                $out_string = mb_detect_encoding($value, array("ASCII", "UTF-8", "GB2312", "GBK", "BIG5"));
+                $rows[] = iconv($out_string, "GBK", $value);
+            }
+            fputcsv($fp, $rows);
+        }
+        // 将已经写到csv中的数据存储变量销毁，释放内存占用
+        unset($export_data);
+        ob_flush();
+        ob_clean();
+        flush();
+        die();
+    }
+}
